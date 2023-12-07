d0722acedd32376dee7f0051773544193868d491
Kozlov Oleg <ko@mann-ivanov-ferber.ru>
2023-12-07 15:55:21 +0200

    add unittests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from main import BooksCollector
 
+
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -8,7 +9,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -18,7 +19,34 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    # №1 Добавление новой книги в словарь без указания жанра
+    def test_add_new_book(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавление новой книги
+        collector.add_new_book('Гарри Поттер')
+        assert len(collector.get_books_genre()) == 1
+
+
+    # №2 проверка добавление книги в фавориты
+    def test_add_book_in_favorites(self):
+        favorites = BooksCollector()
+        name = "Гарри Поттер"
+
+        favorites.add_book_in_favorites(name)
+        assert favorites.get_books_genre() != None
+
+
+    # №3 Проверка наличия жанра у книги
+    def test_add_new_book_set_book_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book("Гарри Поттер")
+        collector.set_book_genre("Гарри Поттер", "Фантастика")
+        assert collector.get_book_genre("Гарри Поттер") == "Фантастика"
